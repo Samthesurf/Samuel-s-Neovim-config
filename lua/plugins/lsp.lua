af22b5c121c86cf2c0f54161212e80df0d3b1c7a
Samthesurf <ukpsamuel67@gmail.com>
2023-12-17 02:52:54 +0100

    Fixing mistakes

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,5 @@
 return {
-    {
+    
         {
             "neovim/nvim-lspconfig",
             config = function()
@@ -69,32 +69,4 @@ return {
                 })
             end,
         },
-        {
-            "williamboman/mason.nvim",
-            dependencies = { "williamboman/mason-lspconfig.nvim" },
-            opts = function()
-                ensure_installed = {
-                    "pyright",
-                    "ruff",
-                    "ruff-lsp",
-                    "jedi-language-server",
-                    "gopls",
-                    "black",
-                    "lua-language-server",
-                    "debugpy",
-                    "html-lsp",
-                    "emmet-language-server",
-                    "vale",
-                    "codelldb",
-                }
-                return ensure_installed
-            end,
-            config = function(_, opts)
-                -- require("mason").setup(opts)
-                vim.api.nvim_create_user_command("MasonInstallAll", function()
-                    vim.cmd("MasonInstall " .. table.concat(ensure_installed, " "))
-                end, {})
-            end,
-        },
-    },
 }
